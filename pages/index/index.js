a2d636997dd8691338b97492708424de6100b144
549492832@qq.com <Qq5494983>
2019-01-30 18:14:03 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,35 @@ const app = getApp()
 
 Page({
   data: {
- 
+     QAList: [
+         { Q: '1', drop:false, className:'one'},
+         { Q: '2', drop: false, className: 'two'},
+         { Q: '3', drop: false, className: 'three'} 
+        ]
   },
   
   onLoad: function () {
    
-  }
+  },
+    tap(e){
+        
+        const item = e.target.dataset.item;
+        const index = e.target.dataset.index;
+        const key = 'QAList[' + index + '].drop';
+        let arr = [...this.data.QAList];
+        
+        this.setData({
+            [key]:true
+        })
+        
+        setTimeout(()=>{
+            arr.unshift(arr.splice(arr.length - 1, 1)[0]);
+            arr.forEach(arrItem => {
+                arrItem.drop = false
+            })
+            this.setData({
+                QAList: arr
+            })
+        },500)
+    }
 })
