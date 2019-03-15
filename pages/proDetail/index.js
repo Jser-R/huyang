b04a2b5d40337fd6b35d5ccd65ed820141969634
549492832@qq.com <Qq5494983>
2019-03-15 13:19:56 +0800

    update

--- pages/proDetail/index.js
+++ pages/proDetail/index.js
@@ -21,33 +21,38 @@ Page({
                 {key:'公司规模',value:'499+'},
                 {key:'所属部门',value:'技术研发部'},
             ],
-            programArr:{
-                //项目版本
-                versions:[
-                    {
-                        hidden:false,
-                        version:'2.2.0',
-                        activeIndex:1,
-                        images:[
-                            imgUrl,
-                            imgUrl,
-                            imgUrl,
-                            imgUrl,
-                        ]
-                    },
-                    {
-                        hidden:true,
-                        version:'2.1.0',
-                        activeIndex:1,
-                        images:[
-                            imgUrl,
-                            imgUrl,
-                            imgUrl,
-                            imgUrl,
-                        ]
-                    }
-                ],
-            }
+            programArr:[
+                {
+                    hidden:false,
+                    version:'2.2.0',
+                    activeIndex:1,
+                    images:[
+                        imgUrl,
+                        imgUrl,
+                        imgUrl,
+                        imgUrl,
+                    ]
+                },
+                {
+                    hidden:true,
+                    version:'2.1.0',
+                    activeIndex:1,
+                    images:[
+                        imgUrl,
+                        imgUrl,
+                        imgUrl,
+                        imgUrl,
+                    ]
+                }
+            ],
+            workDetail:[
+                '1.包拯项目后期的品牌修正和调整；' ,
+                '2.包拯小程序2.1.0全部页面设计及2.2.0的迭代；' ,
+                '3.包拯项目活动页及部分平面的设计；',
+                '4.参与前期产品定位及框架搭建，后期产品上线的… 分析；',
+                '5.定期组织设计团队内部的分享评论会，让一些年轻 设计师更好的把握设计技巧及公司的设计定位；',
+                '6.协助CTO进行产品输出，配合技术完成开发；',
+            ],
         }
 
     },
@@ -76,7 +81,7 @@ Page({
     slideUpVersion(e){
         const item = e.target.dataset.item;
         const index = e.target.dataset.index;
-        const obj = 'proDetail.programArr.versions[' +index+ '].hidden';
+        const obj = 'proDetail.programArr[' +index+ '].hidden';
         if(item.hidden){
             this.setData({
                 [obj]:false
